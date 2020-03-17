9be48fd3d2a1bb8de3c8ad0cef845bc853fa1b4c
Augusto-Amstalden <a213364@dac.unicamp.br>
2020-03-17 16:08:07 -0300

    versao final

--- src/main.c
+++ src/main.c
@@ -1,14 +1,64 @@
-/* Data de submissao:
- * Nome:
- * RA:
+/* Data de submissao: 17/03/2020
+ * Nome: Augusto Piato Amstalden
+ * RA: 213364
  */
 
 #include <stdio.h>
-const int tam_buffer=100;
+#define MAX 100
+
+char pilha[MAX];
+int inicio, fim;
+
+int pilhaCheia() {
+	return (fim == MAX); 	// caso o final for igual a MAX esta cheia
+}
+
+int pilhaVazia() {
+	return (inicio == fim); 	// verifica se esta vazio
+}
+
+void push(char c) {
+	if(pilhaCheia() == 0) {
+		pilha[fim++] = c;		// adiciona um elemento à pilha
+	}
+}
+
+int pop() {
+	char aux;
+	if(pilhaVazia() == 0) {
+		aux = pilha[fim - 1];		// remove um elemento da pilha
+		fim--;
+
+		return aux;		// retorna o elemento devolvido
+		}
+	}
 
 int main() {
-  char buffer[tam_buffer];
-  fgets(buffer, tam_buffer, stdin);
-  printf("%s", buffer);
+
+	char aux;
+  char buffer[MAX];
+  fgets(buffer, MAX, stdin);
+
+	for(int i = 0; i <= MAX; i++) {
+		if(buffer[i] == '\0') {		// caso a entrada ja tinha sido inteira lida
+			printf("\n");
+			break;
+		}
+
+		if((buffer[i] != ' ') && (buffer[i] != '\n')) {	// se estiver na mesma palavra da entrada
+			push(buffer[i]);
+		}
+		else {
+			while(pilhaVazia() == 0) {	// printa a palava ao contrario
+				aux = pop();
+				printf("%c", aux);
+			}
+			if(buffer[i] != '\n') {
+				printf(" ");		// da espaço entre as palavras
+			}
+		}
+	}
+
   return 0;
 }
+
